117996555236135fb0a305cc74ce18b12c9739f6
vantyse <nonsokingsley61@gmail.com>
2022-09-26 22:39:57 +0100

    update proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5001',
+      '/api': 'https://vantyse-calendar.herokuapp.com',
     },
     port: 3000
   }
